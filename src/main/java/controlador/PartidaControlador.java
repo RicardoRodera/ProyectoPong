81a12ff72f2126efc4dec2b8c8972655701a0211
Javier Cabezas <javiercp69@educastur.es>
2022-05-25 22:43:39 +0200

    Pequeños avances

--- src/main/java/controlador/PartidaControlador.java
+++ src/main/java/controlador/PartidaControlador.java
@@ -1,5 +1,23 @@
 package controlador;
 
-public class PartidaControlador {
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class PartidaControlador extends Scene {
+
+    Pala palaJugador;
+    Bola bola;
+
+    public PartidaControlador(Parent parent, double v, double v1, Paint paint) {
+        super(parent, 800, 600, Color.BLACK);
+        this.palaJugador = new Pala();
+        this.bola = new Bola();
+
+    }
+
+
+
 
 }
--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -3,6 +3,7 @@ package modelo;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +11,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Pane root = new Pane();
         Scene scene = new Scene(root, 800, 600);
+        scene.setFill(Color.BLACK);
         stage.setTitle("Proyecto Pong");
         stage.setScene(scene);
         stage.show();
