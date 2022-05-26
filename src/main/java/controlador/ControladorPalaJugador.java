8bd3cd328b9df2b715c8d382d9bfc6802f4ffca6
RicardoRoderaClase <jew16373@educastur.es>
2022-05-26 13:58:17 +0200

    Todo lo de PalaJugador se ha movido a ControladorPalaJugardor, esta es la clase que se encarga de la pala derecha (la que se mueve con el teclado)

--- src/main/java/controlador/ControladorPalaJugador.java
+++ src/main/java/controlador/ControladorPalaJugador.java
@@ -1,7 +1,32 @@
 package controlador;
 
+import javafx.scene.shape.Rectangle;
+import modelo.HelpTools;
+
 public class ControladorPalaJugador {
 
-    public static void resetPosicion() {
+    private static final double alto = 100;
+    private static final double ancho = 10;
+
+    private int velocidad;
+    private Rectangle rectangulo;
+
+    public ControladorPalaJugador() {
+        this.velocidad = 0;
+        this.rectangulo = new Rectangle(HelpTools.WIDTH * 0.85, HelpTools.HEIGHT / 2 - alto / 2, ancho, alto);
+        this.rectangulo.setFill(HelpTools.COLOR_ITEMS);
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public Rectangle getRectangulo() {
+        return rectangulo;
+    }
+
+    private void resetPosicion() {
+        this.rectangulo.setX(HelpTools.WIDTH * 0.85);
+        this.rectangulo.setY(HelpTools.HEIGHT / 2 - alto / 2);
     }
 }
