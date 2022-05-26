8cca40297fdc34459b8d165427c19a14c69343f8
Javier Cabezas <javiercp69@educastur.es>
2022-05-26 18:53:02 +0200

    Pequeña modificacion

--- src/main/java/modelo/Bola.java
+++ src/main/java/modelo/Bola.java
@@ -4,10 +4,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Bola extends Circle{
-    private static final double RADIO = 50;
+    private static final double RADIO = 10;
 
     public Bola() {
-        super(300, -400, RADIO, Color.WHITE);
+        super(300, 400, RADIO, HelpTools.COLOR_ITEMS);
     }
 
     public double getPosicionEjeX() {
