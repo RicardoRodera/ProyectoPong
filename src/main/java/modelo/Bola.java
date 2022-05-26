0b038146fb66a1255b25f9c1d6d27e91edd5100c
Raúl <raulgr74@educastur.es>
2022-05-26 13:37:19 +0200

    Cambiado el color de la bola de negro a blanco

--- src/main/java/modelo/Bola.java
+++ src/main/java/modelo/Bola.java
@@ -1,12 +1,13 @@
 package modelo;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Bola extends Circle{
     private static final double RADIO = 2;
 
     public Bola() {
-        super(300, -400, RADIO);
+        super(300, -400, RADIO, Color.WHITE);
     }
 
     public double getPosicionEjeX() {
