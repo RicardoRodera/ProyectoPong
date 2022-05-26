c802d7e575c92723fe6f6d71917247abf8f25388
RicardoRoderaClase <jew16373@educastur.es>
2022-05-26 14:07:05 +0200

    En principio la pala terminada, a espera de testing.

--- src/main/java/controlador/ControladorPalaJugador.java
+++ src/main/java/controlador/ControladorPalaJugador.java
@@ -5,14 +5,13 @@ import modelo.HelpTools;
 
 public class ControladorPalaJugador {
 
-    private static final double alto = 100;
+    private static final double alto = 80;
     private static final double ancho = 10;
 
-    private int velocidad;
+    private static final int velocidad = 20;
     private Rectangle rectangulo;
 
     public ControladorPalaJugador() {
-        this.velocidad = 0;
         this.rectangulo = new Rectangle(HelpTools.WIDTH * 0.85, HelpTools.HEIGHT / 2 - alto / 2, ancho, alto);
         this.rectangulo.setFill(HelpTools.COLOR_ITEMS);
     }
@@ -28,5 +27,13 @@ public class ControladorPalaJugador {
     private void resetPosicion() {
         this.rectangulo.setX(HelpTools.WIDTH * 0.85);
         this.rectangulo.setY(HelpTools.HEIGHT / 2 - alto / 2);
+    }
+
+    public void moverArriba() {
+        this.rectangulo.setY(this.rectangulo.getY() - velocidad);
+    }
+
+    public void moverAbajo() {
+        this.rectangulo.setY(this.rectangulo.getY() + velocidad);
     }
 }
