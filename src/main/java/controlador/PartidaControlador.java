53b0d4e88e6fdaf66c9becf5c59bef40a034ad7d
Javier Cabezas <javiercp69@educastur.es>
2022-05-26 13:17:31 +0200

    Clase Main - Actualizada Clase PartidaControlador - Retorna la escena

--- src/main/java/controlador/PartidaControlador.java
+++ src/main/java/controlador/PartidaControlador.java
@@ -2,22 +2,49 @@ package controlador;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
+import modelo.HelpTools;
 
-public class PartidaControlador extends Scene {
-    private final static double WIDTH = 800;
-    private final static double HEIGHT = 600;
-    private final static Color COLOR_FONDO = Color.BLACK;
-    private final static Color COLOR_ITEMS = Color.WHITE;
-    //Pala palaJugador;
+public class PartidaControlador  {
+
+    //PalaJugador palaJugador;
+    //PalaAutomatica palaAutomatica
     //Bola bola;
+    Scene scene;
 
-    public PartidaControlador(Parent parent, double v, double v1, Paint paint) {
-        super(parent, 800, 600, Color.BLACK);
-      //  this.palaJugador = new Pala();
-       // this.bola = new Bola();
+    public Scene getScene() {
+        return scene;
+    }
 
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public PartidaControlador() {
+        this.scene = crearScenePartida();
+        //this.palaJugador = new PalaJugador();
+        //this.palaAutomatica = new PalaAutomatica();
+        //this.bola = new Bola();
+    }
+
+    private Scene crearScenePartida(){
+        Pane root = new Pane();
+        Scene scene = new Scene(root, HelpTools.WIDTH, HelpTools.HEIGHT);
+        scene.setFill(HelpTools.COLOR_FONDO);
+        for(int i = 0;
+            i < HelpTools.WIDTH;
+            i += 50){
+            //parametros line - startX, startY, endX, endY
+            Line line  = new Line(HelpTools.WIDTH/2, i, HelpTools.WIDTH/2, i +10);
+            line.setStroke(HelpTools.COLOR_ITEMS);
+            line.setStrokeWidth(10);
+            root.getChildren().add(line);
+        }
+
+        return scene;
     }
 
 
--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -1,29 +1,27 @@
 package modelo;
 
+import controlador.PartidaControlador;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
-import modelo.HelpTools;
+
 /*
 Esta clase es la que inicia la aplicacion y crea la ventana
  */
 
 public class Main extends Application {
 
-   // private final static double WIDTH = 800;
-   // private final static double HEIGHT = 600;
-   // private final static Color COLOR_FONDO = Color.BLACK;
-   // private final static Color COLOR_ITEMS = Color.WHITE;
 
+   static PartidaControlador partidaControlador = new PartidaControlador();
     @Override
     public void start(Stage stage) throws Exception {
 
         stage.setTitle("Proyecto Pong");
         // TODO: 26/5/22 Llamar desde esta clase a la escena que contiene el menu de inicio
-        stage.setScene(partida());
+        stage.setScene(partidaControlador.getScene());
         stage.show();
     }
 
@@ -31,21 +29,6 @@ public class Main extends Application {
         launch();
     }
 
-    // TODO: 26/5/22 trasladar este codigo a la clase PartidaControlador
-    private static Scene partida(){
-        Pane root = new Pane();
-        Scene scene = new Scene(root, HelpTools.WIDTH, HelpTools.HEIGHT);
-        scene.setFill(HelpTools.COLOR_FONDO);
-        for(int i = 0;
-            i < HelpTools.WIDTH;
-            i += 50){
-            //parametros line - startX, startY, endX, endY
-            Line line  = new Line(HelpTools.WIDTH/2, i, HelpTools.WIDTH/2, i +10);
-            line.setStroke(HelpTools.COLOR_ITEMS);
-            line.setStrokeWidth(10);
-            root.getChildren().add(line);
-        }
 
-        return scene;
-    }
+
 }
