303d47e39e2010cc91013308fb1fa9960cedd29b
Javier Cabezas <javiercp69@educastur.es>
2022-05-26 12:51:34 +0200

    Clase Main - Pequeños avances Clase HelpTools - creada y funcionando

---
+++ src/main/java/modelo/HelpTools.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import javafx.scene.paint.Color;
+
+public class HelpTools {
+
+    public final static double WIDTH = 800;
+    public final static double HEIGHT = 600;
+    public final static Color COLOR_FONDO = Color.BLACK;
+    public final static Color COLOR_ITEMS = Color.WHITE;
+
+}
--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -6,15 +6,18 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import modelo.HelpTools;
 /*
 Esta clase es la que inicia la aplicacion y crea la ventana
  */
 
 public class Main extends Application {
-    private final static double WIDTH = 800;
-    private final static double HEIGHT = 600;
-    private final static Color COLOR_FONDO = Color.BLACK;
-    private final static Color COLOR_ITEMS = Color.WHITE;
+
+   // private final static double WIDTH = 800;
+   // private final static double HEIGHT = 600;
+   // private final static Color COLOR_FONDO = Color.BLACK;
+   // private final static Color COLOR_ITEMS = Color.WHITE;
+
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -31,14 +34,14 @@ public class Main extends Application {
     // TODO: 26/5/22 trasladar este codigo a la clase PartidaControlador
     private static Scene partida(){
         Pane root = new Pane();
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
-        scene.setFill(COLOR_FONDO);
+        Scene scene = new Scene(root, HelpTools.WIDTH, HelpTools.HEIGHT);
+        scene.setFill(HelpTools.COLOR_FONDO);
         for(int i = 0;
-            i < WIDTH;
+            i < HelpTools.WIDTH;
             i += 50){
             //parametros line - startX, startY, endX, endY
-            Line line  = new Line(WIDTH/2, i, WIDTH/2, i +10);
-            line.setStroke(COLOR_ITEMS);
+            Line line  = new Line(HelpTools.WIDTH/2, i, HelpTools.WIDTH/2, i +10);
+            line.setStroke(HelpTools.COLOR_ITEMS);
             line.setStrokeWidth(10);
             root.getChildren().add(line);
         }
