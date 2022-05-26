4552dd15176db78ab9dda022d83cb3bc54dab694
Raúl <raulgr74@educastur.es>
2022-05-26 13:57:12 +0200

    Aumentado tamaño bola

--- src/main/java/modelo/Bola.java
+++ src/main/java/modelo/Bola.java
@@ -4,7 +4,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Bola extends Circle{
-    private static final double RADIO = 2;
+    private static final double RADIO = 50;
 
     public Bola() {
         super(300, -400, RADIO, Color.WHITE);
