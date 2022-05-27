3a75dbb5994a53264eb1a192217a4d6a610be5eb
Javier Cabezas <javiercp69@educastur.es>
2022-05-27 09:39:50 +0200

    Main - Creado metodo para cargar las escenas

--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -2,6 +2,7 @@ package modelo;
 
 import controlador.PartidaControlador;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /*
@@ -11,18 +12,39 @@ Esta clase es la que inicia la aplicacion y crea la ventana
 public class Main extends Application {
 
 
+    private final static int MENU_INICIO = 0;
+    private final static int PARTIDA = 1;
+    private final static int PAUSA = 2;
+    private final static int FINAL_PARTIDA = 3;
+    private static int semaforo = PARTIDA;
    static PartidaControlador partidaControlador = new PartidaControlador();
     @Override
     public void start(Stage stage) throws Exception {
 
         stage.setTitle("Proyecto Pong");
         // TODO: 26/5/22 Llamar desde esta clase a la escena que contiene el menu de inicio
-        stage.setScene(partidaControlador.getScene());
+        stage.setScene(cargarScene());
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private Scene cargarScene(){
+
+        switch (semaforo){
+            case MENU_INICIO:
+
+            case PARTIDA: return partidaControlador.getScene();
+
+            case PAUSA:
+
+            case FINAL_PARTIDA:
+
+        }
+
+        return null;
     }
 
 
