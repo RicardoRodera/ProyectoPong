87df96f45518b59630749ad459e02929381931ea
DARKSATAN <QIS64129@educastur.es>
2022-05-30 08:42:22 +0200

    escena del final 2 la venganza

--- src/main/java/modelo/EscenaFin.java
+++ src/main/java/modelo/EscenaFin.java
@@ -1,7 +1,7 @@
 
 
 
-package controlador;
+package modelo;
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
@@ -34,18 +34,18 @@ import java.io.FileNotFoundException;
 
 public class EscenaFin{
 
+Scene escena;
+
+this.escena = EndScene();
 
 
-
-
-    @Override
-    public void start(Stage escenaFin) throws FileNotFoundException {
+    public Scene EndScene() {
 
         crearTexto();
         Pane layout = new Pane();
         Text newGame = new Text();
         Text quitGame = new Text();
-        escenaFin.setTitle("PONG");
+
         newGame.setText("U: Nueva Partida");
         newGame.setFont(Font.font("impact", FontWeight.NORMAL, FontPosture.REGULAR, 15));
         newGame.setFill(HelpTools.COLOR_ITEMS);
@@ -59,7 +59,7 @@ public class EscenaFin{
         layout.getChildren().add(quitGame);
         layout.setBackground(new Background(new BackgroundFill(HelpTools.COLOR_FONDO, CornerRadii.EMPTY, Insets.EMPTY)));
         Scene scene = new Scene(layout, HelpTools.WIDTH, HelpTools.HEIGHT);
-        escenaFin.setScene(scene);
+
         scene.setCursor(Cursor.DISAPPEAR);
         quitGame.setLayoutX(350);
         newGame.setLayoutX(350);
@@ -68,68 +68,30 @@ public class EscenaFin{
         layout.getChildren().add(crearTexto());
         crearTexto().setLayoutX(300);
         crearTexto().setLayoutY(50);
-        Pane layout2 = new Pane();
-        layout2.setBackground(new Background(new BackgroundFill(HelpTools.COLOR_FONDO, CornerRadii.EMPTY, Insets.EMPTY)));
-        layout2.getChildren().add(crearTexto());
-        crearTexto().setLayoutX(300);
-        crearTexto().setLayoutY(50);
-        Text easy = new Text();
-        Text medium = new Text();
-        Text hard = new Text();
-        easy.setText("U: Difícil");
-        easy.setFont(Font.font("impact", FontWeight.NORMAL, FontPosture.REGULAR, 15));
-        easy.setFill(HelpTools.COLOR_ITEMS);
-        medium.setText("R: Dark Souls");
-        medium.setFont(Font.font("impact", FontWeight.NORMAL, FontPosture.REGULAR, 15));
-        medium.setFill(HelpTools.COLOR_ITEMS);
-        hard.setText("D: Ikaruga");
-        hard.setFont(Font.font("impact", FontWeight.NORMAL, FontPosture.REGULAR, 15));
-        hard.setFill(HelpTools.COLOR_ITEMS);
-        layout2.getChildren().add(easy);
-        layout2.getChildren().add(medium);
-        layout2.getChildren().add(hard);
-        easy.setLayoutX(350);
-        medium.setLayoutX(350);
-        hard.setLayoutX(350);
-        easy.setLayoutY(350);
-        medium.setLayoutY(400);
-        hard.setLayoutY(450);
-        Scene scene2 = new Scene(layout2, 800, 600);
-        escenaFin.initStyle(StageStyle.DECORATED);
-        escenaFin.show();
-        escenaFin.requestFocus();
+
+
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
-            if(event.getCode() == KeyCode.UP){
-                escenaFin.setScene(scene2);
-                escenaFin.requestFocus();
+            if (event.getCode() == KeyCode.UP) {
+                return scene;
+
 
             }
-            if(event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.ESCAPE){
-                escenaFin.close();
+            if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.ESCAPE) {
+                //close();
+                return scene;
             }
         });
-        scene2.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
-            if(event.getCode() == KeyCode.UP){
-                escenaFin.setScene(scene);
-                escenaFin.requestFocus();
-
-            }
-            if(event.getCode() == KeyCode.RIGHT){
-                escenaFin.setScene(scene);
-                escenaFin.requestFocus();
-            }
-            if(event.getCode() == KeyCode.DOWN){
-                escenaFin.setScene(scene);
-                escenaFin.requestFocus();
-            }
-            if(event.getCode() == KeyCode.ESCAPE){
-                escenaFin.setScene(scene);
-            }
-        });
-
-
-
     }
+
+
+
+
+
+
+
+
+
+
 
     private Node crearTexto() {
         String header = "GAME OVER";
@@ -156,12 +118,6 @@ public class EscenaFin{
 
 
 
-
-
-
-    public static void main (String[]args){
-            Application.launch(args);
-        }
 
 
     }
